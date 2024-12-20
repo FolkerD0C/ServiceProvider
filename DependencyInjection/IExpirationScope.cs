5116df659df3150822d86938ff184bd1c8b5191e
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2024-12-20 13:51:39 +0100

    Update IExpirationScope documentation.

--- DependencyInjection/IExpirationScope.cs
+++ DependencyInjection/IExpirationScope.cs
@@ -1,29 +1,30 @@
 ﻿namespace FolkerD0C.DependencyInjection;
 
 /// <summary>
-/// Defines a contract for objects that have an expiration scope,
-/// specifying their ability to be renewed and their current validity state.
+/// Defines a contract for objects that have an expiration scope.
+/// If a scope is expired then the object is renewed.
+/// If a scope is renewable then it gets renewed.
 /// </summary>
 public interface IExpirationScope
 {
     /// <summary>
-    /// Gets a value indicating whether the object can be renewed.
+    /// Gets a value indicating whether the scope can be renewed.
     /// </summary>
     /// <value>
-    /// <c>true</c> if the object can be renewed; otherwise, <c>false</c>.
+    /// <c>true</c> if the scope can be renewed; otherwise, <c>false</c>.
     /// </value>
     bool IsRenewable { get; }
 
     /// <summary>
-    /// Gets a value indicating whether the object is currently valid.
+    /// Gets a value indicating whether the scope is currently valid.
     /// </summary>
     /// <value>
-    /// <c>true</c> if the object is valid at the current time; otherwise, <c>false</c>.
+    /// <c>true</c> if the scope is valid at the current time; otherwise, <c>false</c>.
     /// </value>
     bool IsValidCurrently { get; }
 
     /// <summary>
-    /// Renews the object, resetting or extending its validity period.
+    /// Renews the scope, resetting or extending its validity period.
     /// </summary>
     void Renew();
 }
