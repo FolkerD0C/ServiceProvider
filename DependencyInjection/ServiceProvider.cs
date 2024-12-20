cd93e5604b146ed9a91df37e1cb25980a0702638
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2024-12-20 13:38:34 +0100

    Implement ServiceProvider and ServiceProviderBuilder

--- DependencyInjection/ServiceProvider.cs
+++ DependencyInjection/ServiceProvider.cs
@@ -1,9 +1,111 @@
-﻿namespace FolkerD0C.DependencyInjection;
+﻿using System.Reflection;
 
+namespace FolkerD0C.DependencyInjection;
+
+/// <summary>
+/// Provides an implementation for the <see cref="IServiceProvider"/> interface.
+/// </summary>
 public class ServiceProvider : IServiceProvider
 {
+    private readonly Dictionary<Type, RegisteredType> _registeredTypes;
+    private readonly Dictionary<Type, object?> _cachedObjects = [];
+    private MethodInfo? _resolveMethod;
+
+    internal ServiceProvider(Dictionary<Type, RegisteredType> registeredTypes)
+    {
+        _registeredTypes = registeredTypes;
+    }
+
+    /// <inheritdoc/>
     public TContract Resolve<TContract>()
     {
-        throw new NotImplementedException();
+        var registeredType = GetRegisteredType<TContract>();
+
+        return InternalResolve(registeredType);
     }
+
+    #region Private methods
+    private TContract Create<TContract>(RegisteredType<TContract> registeredType)
+    {
+        if (registeredType.Instantiator is not null)
+        {
+            return registeredType.Instantiator();
+        }
+
+        if (_resolveMethod == null)
+        {
+            _resolveMethod = GetType().GetMethod("Resolve");
+        }
+
+        var constructorParameters = registeredType.ImplementationType
+            .GetConstructors().Concat(registeredType.ImplementationType.GetConstructors(BindingFlags.NonPublic))
+            .Select(constructorInfo =>
+                new
+                {
+                    Constructor = constructorInfo,
+                    Parameters = constructorInfo.GetParameters()
+                })
+            .OrderByDescending(a => a.Parameters.Length)
+            .First().Parameters
+            .Select(parameterInfo =>
+            {
+                var genericResolveMethod = _resolveMethod?.MakeGenericMethod(parameterInfo.ParameterType);
+                return genericResolveMethod?.Invoke(this, null);
+            })
+            .ToArray();
+
+        object? result = Activator.CreateInstance(registeredType.ImplementationType, constructorParameters);
+
+        return result.GuardedCast<TContract>();
+    }
+
+    private RegisteredType<TContract> GetRegisteredType<TContract>()
+    {
+        if (_registeredTypes.TryGetValue(typeof(TContract), out RegisteredType? registeredType)
+            || (typeof(TContract).IsGenericType
+                && _registeredTypes.TryGetValue(typeof(TContract).GetGenericTypeDefinition(), out registeredType)))
+        {
+            if (registeredType is RegisteredType<TContract> parameterizedRegisteredType)
+                return parameterizedRegisteredType;
+        }
+        throw new InvalidOperationException($"{typeof(TContract)} is not a registered type.");
+    }
+
+    private TContract HandleExpiredScope<TContract>(RegisteredType<TContract> registeredType)
+    {
+        _cachedObjects.Remove(typeof(TContract));
+
+        TContract result = Create(registeredType);
+
+        if (registeredType.Scope.IsRenewable)
+        {
+            registeredType.Scope.Renew();
+            if (registeredType.Scope.IsValidCurrently)
+            {
+                _cachedObjects.Add(typeof(TContract), result);
+            }   
+        }
+
+        return result;
+    }
+
+    private TContract HandleValidScope<TContract>(RegisteredType<TContract> registeredType)
+    {
+        if (_cachedObjects.ContainsKey(typeof(TContract)))
+        {
+            return _cachedObjects[typeof(TContract)].GuardedCast<TContract>();
+        }
+
+        TContract result = Create(registeredType);
+        _cachedObjects.Add(typeof(TContract), result);
+        return result;
+    }
+
+    private TContract InternalResolve<TContract>(RegisteredType<TContract> registeredType)
+    {
+        return registeredType.Scope.IsValidCurrently
+            ? HandleValidScope(registeredType)
+            : HandleExpiredScope(registeredType);
+    }
+    #endregion
 }
--- DependencyInjection/ServiceProviderBuilder.cs
+++ DependencyInjection/ServiceProviderBuilder.cs
@@ -1,42 +1,88 @@
-
 namespace FolkerD0C.DependencyInjection;
 
-public class ServiceProviderBuilder : IServiceProviderBuilder
+/// <summary>
+/// Provides an implementation for the <see cref="IServiceProviderBuilder"/> interface.
+/// </summary>
+public sealed class ServiceProviderBuilder : IServiceProviderBuilder
 {
+    public static readonly IServiceProviderBuilder DefaultInstance = new ServiceProviderBuilder();
+
+    private readonly Dictionary<Type, RegisteredType> _registeredTypes = new();
+
     #region Public methods
+    /// <inheritdoc/>
     public IServiceProviderBuilder AddScoped<TImplementation>(IExpirationScope scope, Func<TImplementation>? instantiator = null)
     {
-        throw new NotImplementedException();
+        Add(typeof(TImplementation), scope, instantiator);
+        return this;
     }
 
+    /// <inheritdoc/>
     public IServiceProviderBuilder AddScoped<TContract, TImplementation>(IExpirationScope scope, Func<TContract>? instantiator = null)
     {
-        throw new NotImplementedException();
+        Add(typeof(TImplementation), scope, instantiator);
+        return this;
     }
 
+    /// <inheritdoc/>
     public IServiceProviderBuilder AddSingleton<TImplementation>(Func<TImplementation>? instantiator = null)
     {
-        throw new NotImplementedException();
+        Add(typeof(TImplementation), ExpirationScope.GetSingleton(), instantiator);
+        return this;
     }
 
+    /// <inheritdoc/>
     public IServiceProviderBuilder AddSingleton<TContract, TImplementation>(Func<TContract>? instantiator = null)
     {
-        throw new NotImplementedException();
+        Add(typeof(TImplementation), ExpirationScope.GetSingleton(), instantiator);
+        return this;
     }
 
+    /// <inheritdoc/>
     public IServiceProviderBuilder AddTransient<TImplementation>(Func<TImplementation>? instantiator = null)
     {
-        throw new NotImplementedException();
+        Add(typeof(TImplementation), ExpirationScope.GetTransient(), instantiator);
+        return this;
     }
 
+    /// <inheritdoc/>
     public IServiceProviderBuilder AddTransient<TContract, TImplementation>(Func<TContract>? instantiator = null)
     {
-        throw new NotImplementedException();
+        Add(typeof(TImplementation), ExpirationScope.GetTransient(), instantiator);
+        return this;
     }
 
+    /// <inheritdoc/>
     public IServiceProvider Build()
     {
-        throw new NotImplementedException();
+        return new ServiceProvider(_registeredTypes);
+    }
+    #endregion
+
+    #region Private methods
+
+    private void Add<TContract>(Type implementationType, IExpirationScope scope, Func<TContract>? instantiator = null)
+    {
+        if (implementationType.IsInterface || implementationType.IsAbstract)
+        {
+            throw new InvalidOperationException($"{implementationType} is not constructable.");
+        }
+
+        if (!implementationType.IsAssignableTo(typeof(TContract)))
+        {
+            throw new InvalidOperationException($"{implementationType} is not assignable to {typeof(TContract)}");
+        }
+
+        if (_registeredTypes.ContainsKey(typeof(TContract)))
+        {
+            return;
+        }
+        if (typeof(TContract).IsGenericType && implementationType.IsGenericTypeDefinition)
+        {
+            implementationType = implementationType.MakeGenericType(typeof(TContract).GenericTypeArguments);
+        }
+
+        _registeredTypes.Add(typeof(TContract), new RegisteredType<TContract>(implementationType, scope, instantiator));
     }
     #endregion
 }
