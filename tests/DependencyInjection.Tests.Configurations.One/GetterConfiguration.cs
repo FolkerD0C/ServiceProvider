d714482c3329b1b661f5aaeb7d6748d2d55973de
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2025-01-11 11:28:26 +0100

    Add simple configuration for tests

---
+++ tests/DependencyInjection.Tests.Configurations.One/GetterConfiguration.cs
@@ -0,0 +1,14 @@
+using FolkerD0C.DependencyInjection.Configuration;
+using FolkerD0C.DependencyInjection.Tests.Shared.Services;
+
+namespace FolkerD0C.DependencyInjection.Tests.AssemblyTests.One;
+
+public class GetterConfiguration<TValue>(TValue gettableValue) : IServiceProviderBuilderConfiguration
+{
+    private readonly TValue _gettableValue = gettableValue;
+
+    public IServiceProviderBuilder Configure(IServiceProviderBuilder builder)
+    {
+        return builder.AddSingleton(() => new GetterService<TValue>(_gettableValue));
+    }
+}
