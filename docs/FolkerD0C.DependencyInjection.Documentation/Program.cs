ca13b2cd12bcbea1b5da50b942bbef4179e1aba9
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2025-01-12 16:58:25 +0100

    Add documentation project

---
+++ docs/FolkerD0C.DependencyInjection.Documentation/Program.cs
@@ -0,0 +1,9 @@
+﻿using FolkerD0C.DependencyInjection;
+using XmlDocMarkdown.Core;
+
+string[] defaultDocumentationArgs = [ typeof(ServiceProvider).Assembly.GetName().Name
+    ?? "FolkerD0C.DependencyInjection" ];
+
+var doucmentationArgs = defaultDocumentationArgs.Concat(args).ToArray();
+
+XmlDocMarkdownApp.Run(doucmentationArgs);
